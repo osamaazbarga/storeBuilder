e0b4d7dec69ac5fee4c36b1b561fa53cd4d61211
Osama Azbarga <osamaazbarga@gmail.com>
2025-05-13 01:38:17 +0300

    side bar ui

--- src/app/private/components/sidebar-dashboard/sidebar-dashboard.component.ts
+++ src/app/private/components/sidebar-dashboard/sidebar-dashboard.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { take } from 'rxjs';
+import { User } from 'src/app/models/account/user';
+import { StoreService } from 'src/app/services/store.service';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-sidebar-dashboard',
@@ -6,6 +11,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar-dashboard.component.css']
 })
 export class SidebarDashboardComponent {
+  errorMessages:string[]=[]
+  mode:string|undefined;
+  storeData:any
+
+  constructor(private storeService:StoreService,private userService:UsersService,private activatedRoute:ActivatedRoute,private router:Router){
+    this.userService.user$.pipe(take(1)).subscribe({
+              next:(user:User|null)=>{
+                if(user){
+                    this.getStoreByUserId(user.id!)
+                }
+                else{
+                  const mode=this.activatedRoute.snapshot.paramMap.get('mode');
+                  if(mode){
+                    this.mode=mode
+                    console.log(this.mode);
+                  }             
+                }
+              }
+        })
+  }
+
+
+  getStoreByUserId(userId:string){
+    this.errorMessages=[];
+    
+
+      this.storeService.getStoreByUserId(userId).subscribe({
+          next:(res:any)=>{
+            if(res){
+              this.storeData=res
+            } 
+            else{
+              this.errorMessages.push("no Stores yet");
+            }     
+            // this.sharedService.showNotification(true,res.value.title,res.value.message);
+            // this.router.navigateByUrl('/login')
+          },
+          error:error=>{
+            if(error.error.errors){
+              this.errorMessages=error.error.errors
+              
+            }
+            else{
+              this.errorMessages.push(error.error)
+            }
+            
+          }
+      })
+    
+  }
+
+  routerLink(){
+    // this.router.navigateByUrl(`https://${this.storeData.link}.localtest.me:4200/`)
+    window.location.href = `https://${this.storeData.link}.localtest.me:4200/`
+   }
+
   itemList = [
     { labelKey: 'MERCHANT.DASHBOARD', icon: 'dashboard', route: '/dashboard' },
     { labelKey: 'MERCHANT.PRODUCTS', icon: 'apparel', route: '/dashboard/products' },
